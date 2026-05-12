a8a850c4fff1c485e863e60547a04c759e4dfc0c
rayannebandeira <rkbr1@aluno.ifnmg.edu.br>
2026-05-12 16:59:13 +0000

    feat: modifiquei o index

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import express from 'express'; // importa o Express usando ES Modules
+import express from 'express';
 
-const app = express(); // cria a aplicação Express
-const PORT = 3000; // porta onde o servidor vai rodar localmente
+const app = express();
+const PORT = 3000;
 
-// rota GET na raiz — responde com JSON
 app.get('/', (req, res) => {
   res.json({ mensagem: 'Yearbook API está no ar! 🎓' });
 });
+
 app.get('/status', (req, res) => {
-  status : "ok",
-  timestamp = new Date
+  res.json({
+    status: "ok",
+    timestamp: new Date()
+  });
 });
 
-// inicia o servidor localmente — na Vercel essa parte é pulada
 if (process.env.VERCEL !== '1') {
   app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
   });
 }
 
-// exporta o app para a Vercel usar como serverless function
 export default app;
